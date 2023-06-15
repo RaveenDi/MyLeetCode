ee3600273e2fc17edb3a41e21ea11f36cc7c2825
Raveen Dissanayaka <rave.dissanayaka@gmail.com>
2023-06-16 01:23:45 +0530

    Merge sort impl

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         int[] array = new int[]{38, 21, 47, 50, 24, 18, 24, 4, 22, 10, 23, 23, 12};
 
         long startTime = System.nanoTime();
-        array = insertionSort(array);
+        array = mergeSort(array);
         long endTime = System.nanoTime();
 
         System.out.print( "Array    : ");
@@ -14,6 +14,63 @@ public class Main {
         float duration = (endTime - startTime);
         System.out.println( "\nDuration : " + duration/1000000);
     }
+
+    public static int[] mergeSort(int[] array) {
+        sort(array, 0, array.length-1);
+        return array;
+    }
+
+    public static void sort(int[] array, int l, int r) {
+        if(l < r) {
+            int m = l + ( r - l ) / 2;
+
+            sort(array, l, m);
+            sort(array, m + 1, r);
+
+            merge(array, l, m, r);
+        }
+    }
+
+    public static void merge(int[] array, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            left[i] = array[l+i];
+        for (int i = 0; i< n2; i++)
+            right[i] = array[m+i+1];
+
+        int i = 0, j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                array[k] = left[i];
+                i++;
+            }
+            else {
+                array[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            array[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            array[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
 
     public static int[] insertionSort(int[] array) {
         int value;
