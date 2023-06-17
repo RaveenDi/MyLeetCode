0eb719b77f948f77c2dd2c9d856e356ed018c8ff
Raveen Dissanayaka <rave.dissanayaka@gmail.com>
2023-06-17 23:19:54 +0530

    New impl to the merge sort

--- src/Main.java
+++ src/Main.java
@@ -30,45 +30,40 @@ public class Main {
             merge(array, l, m, r);
         }
     }
-
     public static void merge(int[] array, int l, int m, int r) {
-        int n1 = m - l + 1;
-        int n2 = r - m;
+        int leftSize = m - l + 1;
+        int rightSize = r - m;
 
-        int[] left = new int[n1];
-        int[] right = new int[n2];
+        int[] left = new int[leftSize];
+        int[] right = new int[rightSize];
 
-        for (int i = 0; i < n1; i++)
-            left[i] = array[l+i];
-        for (int i = 0; i< n2; i++)
-            right[i] = array[m+i+1];
+        for(int i = 0; i < leftSize; i++)
+            left[i] = array[l + i];
+        for (int j = 0; j < rightSize; j++)
+            right[j] = array[m + 1 + j];
 
-        int i = 0, j = 0;
+        int i = 0,j=0;
 
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (left[i] <= right[j]) {
-                array[k] = left[i];
+        while ((i< leftSize) && (j<rightSize)) {
+            if (left[i] < right[j]) {
+                array[l + i + j] = left[i];
                 i++;
-            }
-            else {
-                array[k] = right[j];
+            } else {
+                array[l + i + j] = right[j];
                 j++;
             }
-            k++;
         }
 
-        while (i < n1) {
-            array[k] = left[i];
+        while (i< leftSize) {
+            array[l + i + j] = left[i];
             i++;
-            k++;
         }
 
-        while (j < n2) {
-            array[k] = right[j];
+        while (j<rightSize) {
+            array[l + i + j] = right[j];
             j++;
-            k++;
         }
+
     }
 
 
